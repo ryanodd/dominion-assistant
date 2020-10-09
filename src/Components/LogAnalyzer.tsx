0638dde94f6a0ae595fb7e1579b181c830a846de
Ryan Odd <ryanodd@gmail.com>
2020-10-08 21:43:56 -0400

    Reformat deckInfo skeleton (rough)

--- src/Components/LogAnalyzer.tsx
+++ src/Components/LogAnalyzer.tsx
@@ -3,14 +3,26 @@ import { Row, Col } from 'jsxstyle';
 import { CardContainer } from './CardContainer';
 import { RequestService } from '../Services/RequestService';
 import { PasteLogBox } from './PasteLogBox';
+import { Divider } from 'antd';
 
 interface LogAnalyzerProps {
   //code related to your props goes here
 }
 
+interface DeckInfo {
+  playerName: string //?
+  playerInitial: string //?
+  cardNameList: string[]
+  numCards: number
+  totalMoney: number
+  totalStops: number
+  totalDraw: number
+  totalTerminals: number
+  totalVillages: number
+}
+
 interface LogAnalyzerState {
-  deck1List: string[];
-  deck2List: string[];
+  deckInfos: DeckInfo[]
 }
 
 // This component is super stateful
@@ -19,25 +31,34 @@ export class LogAnalyzer extends React.Component<LogAnalyzerProps, LogAnalyzerSt
   constructor(props: Object) {
     super(props);
     this.state = {
-      deck1List: [],
-      deck2List: [],
+      deckInfos: [],
     }
-    //init state
   }
 
   pasteCallback = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    // start loading animation
+    // TODO: start loading animation
     RequestService.logPasteRequest(event.target.value)
     .then((payload) => {
       this.setState({
-        deck1List: payload.deck1List,
-        deck2List: payload.deck2List,
+        deckInfos: payload.deckInfos,
       });
     })
     .catch(() => {
       console.log("ERROR!!!!!!!!! logPasteRequest failed")
     })
+  }
 
+  renderDeckInfos = () => {
+    deckInfosToRender = []
+    deckInfosToRender += 
+      <Row fontSize={16}>
+      {'Deck 2'}
+      <CardContainer
+        cardNameList={this.state.deck2List}
+        style={{'backgroundColor': '#a0a0a0'}}
+      />
+      </Row>
+    return deckInfosToRender
   }
 
   render(): any {
@@ -51,20 +72,8 @@ export class LogAnalyzer extends React.Component<LogAnalyzerProps, LogAnalyzerSt
         <PasteLogBox
           pasteCallback={this.pasteCallback}
         />
-        <Row fontSize={16}>
-          {'Deck 1'}
-        </Row>
-        <CardContainer
-          cardNameList={this.state.deck1List}
-          style={{'backgroundColor': '#a0a0a0'}}
-        />
-        <Row fontSize={16}>
-          {'Deck 2'}
-        </Row>
-        <CardContainer
-          cardNameList={this.state.deck2List}
-          style={{'backgroundColor': '#a0a0a0'}}
-        />
+        <Divider/>
+        {this.renderDeckInfos()}
       </Col>
     );
   }
