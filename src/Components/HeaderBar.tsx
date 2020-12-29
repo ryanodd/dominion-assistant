0eb671f4ec13d16398c4fa724cb4f6692a90c40b
Ryan Odd <ryanodd@gmail.com>
2020-12-28 19:05:29 -0500

    Added info blurb for unfamiliar users

--- src/Components/HeaderBar.tsx
+++ src/Components/HeaderBar.tsx
@@ -1,9 +1,20 @@
 import React, { FunctionComponent } from "react";
 import { Row, Col } from "jsxstyle";
+import { Popover } from "antd";
+import { InfoCircleFilled } from "@ant-design/icons";
 
 interface HeaderBarProps {
   style?: {}
 };
+
+const infoTooltipContent = (
+  <Col
+    width={180}
+  >
+  {"Council Room is a deck tracking tool for the card game 'Dominion Online'. Just paste your game's log, and each player's stats will appear."}
+  </Col>
+)
+
 
 export const HeaderBar: FunctionComponent<HeaderBarProps> = ({style}) => {  
   return (
@@ -12,6 +23,7 @@ export const HeaderBar: FunctionComponent<HeaderBarProps> = ({style}) => {
           ...style, 
           'backgroundColor': '#202020',
           'padding': 10,
+          'justifyContent': 'space-between',
         }}
       >
         <Col
@@ -20,6 +32,22 @@ export const HeaderBar: FunctionComponent<HeaderBarProps> = ({style}) => {
         >
           {'Council Room'}
         </Col>
+          <Col
+            fontSize={16}
+            color='#E0E0E0'
+            justifyContent='center'
+          >
+            <Popover
+              arrowPointAtCenter
+              placement='bottomRight'
+              content={infoTooltipContent}
+            >
+              <span>
+                <InfoCircleFilled/>
+                {' What is this?'}
+              </span>
+            </Popover>
+          </Col>
       </Row>
   );
 }
