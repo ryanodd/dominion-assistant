9a3aed0648f3179307fbd3fcfa5a887e46132d34
Ryan Odd <ryan@stageten.tv>
2021-07-17 18:09:06 -0400

    remove missing apple touch icon logo

--- src/Components/LogAnalyzer.tsx
+++ src/Components/LogAnalyzer.tsx
@@ -25,7 +25,7 @@ const LogAnalyzer = () => {
       + '?subject=Dominion%20Issue'
       + '&body='
       + ((gameLog || returnPayload || error) ? '%0D%0A%0D%0A%2D%2D%2D%2D%2D%2D%0D%0A%0D%0A' : '')
-      + (gameLog ? `Log:%0D%0A${gameLog.replaceAll('\n', '%0D%0A')}%0D%0A%0D%0A` : '')
+      + (gameLog ? `Log:%0D%0A${gameLog}%0D%0A%0D%0A` : '')
       + (returnPayload ? `Return:%0D%0A${JSON.stringify(returnPayload)}%0D%0A%0D%0A` : '')
       + (error ? `Error:%0D%0A${JSON.stringify(error)}%0D%0A%0D%0A` : '')
     )
